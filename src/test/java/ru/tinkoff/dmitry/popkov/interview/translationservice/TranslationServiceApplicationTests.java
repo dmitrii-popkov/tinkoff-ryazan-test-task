71324f29b06731d86ebb59c5d65e998acf9efe32
Dmitrii Popkov <Dmitrii_Popkov@epam.com>
2022-03-16 12:50:51 +0300

    Initial commit

---
+++ src/test/java/ru/tinkoff/dmitry/popkov/interview/translationservice/TranslationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.dmitry.popkov.interview.translationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
